9012e4a4cf4121a240761bab4119fa293890eea2
Quin Schaller <qaschaller@gmail.com>
2024-08-27 09:44:12 -0500

    New Stuff

--- Persistance/ContentView.swift
+++ Persistance/ContentView.swift
@@ -10,11 +10,40 @@ import SwiftData
 
 struct ContentView: View {
     
+    @Environment(\.modelContext) var context
+    @Query var Colleges: [College]
+    @State var collegeName = ""
     
     var body: some View {
         VStack{
             
+            TextField("Enter Name", text: $collegeName)
+                .textFieldStyle(.roundedBorder)
+            Button("+") {
+                let college = College(name: collegeName)
+                context.insert(college)
+                collegeName = ""
+            }
+            .font(.largeTitle)
             
         }
+        .padding()
+        
+        
+        List{
+                    ForEach(Colleges) { currentCollege in
+                        Text(currentCollege.name)
+                    }
+                }
+    }
+}
+
+
+@Model
+class College {
+    var name: String
+    
+    init(name:String) {
+        self.name = name
     }
 }
--- Persistance/PersistanceApp.swift
+++ Persistance/PersistanceApp.swift
@@ -13,6 +13,7 @@ struct PersistanceApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: College.self)
         }
     }
 }
